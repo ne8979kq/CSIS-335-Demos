d33ea09ecb23c6f0efa9b46e1aa79ec0c237326f
Ronni Kurtzhals <45405916+ne8979kq@users.noreply.github.com>
2025-09-17 15:19:21 -0500

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.nodespropertiesbindings {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.nodespropertiesbindings to javafx.fxml;
-    exports com.example.nodespropertiesbindings;
-}
